965ac2af884b4e22f7aeb14a47019f682bd766e8
Devxhkl <devxhkl@outlook.com>
2015-08-09 22:00:52 +0200

    Cleaning

--- SwiftNotificationBannerDemo/ViewController.swift
+++ SwiftNotificationBannerDemo/ViewController.swift
@@ -13,17 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-//        view.addSubview(navigationBanner)
-        
-        
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
         SwitNotificationBanner.presentNotification("message")
-//        performSegueWithIdentifier("show", sender: self)
     }
     
     override func prefersStatusBarHidden() -> Bool {
@@ -32,7 +27,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
